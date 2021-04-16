b35eebae0fa6f4cc32d25b26a2ea9452ba3485d0
Midhet Sulemani <midhetfatema94@gmail.com>
2021-04-16 16:16:48 +0300

    added UI framework

---
+++ ReKindle/AddNewBookViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddNewBookViewController.swift
+//  ReKindle
+//
+//  Created by Midhet Sulemani on 4/16/21.
+//
+
+import UIKit
+
+class AddNewBookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
